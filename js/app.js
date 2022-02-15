03da218c40d884e5d773ef5354690d1115375acc
bsgarcia <basilegarcia@gmail.com>
2022-02-15 12:39:12 +0100

    update

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ function main() {
     let exp = new ExperimentParameters(
         {
             online: true,   // send network requests
-            isTesting: false, // isTesting==in development vs in production
+            isTesting: true, // isTesting==in development vs in production
             expName: 'Feedback1', // experience name
             completeFeedback: true, // display feedback of both options
             maxPoints: undefined, // max points cumulated all along the experiment
