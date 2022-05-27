5fb43e1b2d77902a5178ed6751c309729096db9d
bsgarcia <basilegarcia@gmail.com>
2022-05-27 13:50:21 +0200

    update

--- js/modules/trial_manager.js
+++ js/modules/trial_manager.js
@@ -171,9 +171,9 @@ export class ChoiceManager {
 
         let p1 = params['p1'];
         let p2 = params['p2'];
-        console.log('Trial: ' + this.trialNum);
-        console.log(p1);
-        console.log(p2);
+        // console.log('Trial: ' + this.trialNum);
+        // console.log(p1);
+        // console.log(p2);
 
         let symL;
         let symR;
@@ -472,8 +472,8 @@ export class SliderManager {
         let isCatchTrial = +(params["isCatchTrial"]);
         let option1Type = params['option1Type'];
 
-        console.log('Trial: ' + this.trialNum);
-        console.log(p1);
+        // console.log('Trial: ' + this.trialNum);
+        // console.log(p1);
 
 
 
