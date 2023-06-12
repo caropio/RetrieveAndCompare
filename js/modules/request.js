30752b01f16d3e77aebef7deacb8f34afdd70c09
bsgarcia <basilegarcia@gmail.com>
2023-06-12 20:21:11 +0200

    updarte

--- js/modules/request.js
+++ js/modules/request.js
@@ -1,23 +1,53 @@
-import { GUI } from './gui.js'
-// import axios from './../../lib/js/axios.min.js';
+import {GUI} from './gui.js'
 
 const MAX_REQUESTS = 3;
 
+
 export function sendToDB(call, data, url) {
-    axios.post(url, data)
-        .then(response => {
-            const r = response.data;
+    $.ajax({
+        type: 'POST',
+        data: data,
+        async: true,
+        url: url,
+        success: function (r) {
+
             if (r.error > 0 && (call + 1) < MAX_REQUESTS) {
-                sendToDB(call + 1, data, url);
+                sendToDB(call + 1);
             }
-        })
-        .catch(error => {
+        },
+
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+
             if ((call + 1) < MAX_REQUESTS) {
-                sendToDB(call + 1, data, url);
+                sendToDB(call + 1);
             } else {
                 GUI.displayModalWindow('Network error',
                     `Please check your internet connection.\n\n
-           If you are not online, the data is lost and we can\'t pay you. :(`, 'error');
+                     If you are not online, the data is lost and we can\'t pay you. :(`, 'error');
             }
-        });
+
+        }
+
+    });
 }
+
+
+//export function sendToDB(call, data, url) {
+//    axios.post(url, data)
+//        .then(response => {
+//            const r = response.data;
+//            if (r.error > 0 && (call + 1) < MAX_REQUESTS) {
+//                sendToDB(call + 1, data, url);
+//            }
+//        })
+//        .catch(error => {
+//            if ((call + 1) < MAX_REQUESTS) {
+//                sendToDB(call + 1, data, url);
+//            } else {
+//                GUI.displayModalWindow('Network error',
+//                    `Please check your internet connection.\n\n
+//           If you are not online, the data is lost and we can\'t pay you. :(`, 'error');
+//            }
+//        });
+//}
+
--- php/InsertLearningDataDB.php
+++ php/InsertLearningDataDB.php
@@ -2,10 +2,6 @@
 
 include 'connectDB.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $EXP         = stripslashes(htmlspecialchars($_POST['exp']));
 $EXPID         = stripslashes(htmlspecialchars($_POST['expID']));
 $ID         = stripslashes(htmlspecialchars($_POST['id']));
