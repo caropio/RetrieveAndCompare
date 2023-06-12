a434113d4bd9ca710f4e36af821f94041d15ad49
bsgarcia <basilegarcia@gmail.com>
2023-06-12 19:39:17 +0200

    updarte

--- php/InsertLearningDataDB.php
+++ php/InsertLearningDataDB.php
@@ -33,7 +33,7 @@ $CTIME 		= stripslashes(htmlspecialchars($_POST['choice_time']));
 $DIST 		= stripslashes(htmlspecialchars($_POST['elic_distance']));
 $PLOT 		= stripslashes(htmlspecialchars($_POST['p_lottery']));
 
-$stmt = $db->prepare("INSERT INTO r_and_c_learning_data VALUE(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())");
+$stmt = $db->prepare("INSERT INTO r_and_c_test VALUE(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())");
 $stmt->bind_param("sssiddidiiiiiiiissidiiiddiiiid",
     $EXP,$EXPID,$ID, $ELIC, $P1,$P2, $RTIME, $OUT, $CF_OUT, $CHOICE, $CORRECT_CHOICE, $TEST,$TRIAL,$COND, $CONT1, $CONT2, $SYML,$SYMR,$LR,$REW,$SESSION,$OP1,$OP2, $EV1, $EV2, $CATCH, $INV,$CTIME, $DIST, $PLOT
 );
