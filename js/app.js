1457fe0c2d36d8164910091673a8b0f4938063e2
caropio <carolinepioger98@gmail.com>
2025-03-06 15:46:00 +0100

    changing expname to 2 and outcomeTypes to change session order

--- js/app.js
+++ js/app.js
@@ -39,8 +39,8 @@ function main() {
     let exp = new ExperimentParameters(
         {
             online: true,   // send network requests
-            isTesting: false, // isTesting==in development vs in production
-            expName: 'EvOutcomeNoFixed1', // experience name
+            isTesting: true, // isTesting==in development vs in production
+            expName: 'EvOutcomeNoFixed2', // experience name
             completeFeedback: true, // display feedback of both options
             maxPoints: undefined, // max points cumulated all along the experiment
             // if undefined or 0, will be computed automatically
@@ -279,7 +279,8 @@ function stateMachine({ instructionNum, sessionNum, phaseNum, questNum, exp } = 
                     beforeFeedbackDuration: exp.beforeFeedbackDuration,
                     completeFeedback: exp.completeFeedback,
                     feedbackObj: exp.feedbackImg,
-                    outcomeType: [0, ''][+([1].includes(sessionNum))],
+                    // outcomeType: [0, ''][+([1].includes(sessionNum))],
+                    outcomeType: [0, ''][+([-2, -1, 0].includes(sessionNum))],
                     imgObj: imgObj,
                     sessionNum: sessionNum,
                     phaseNum: phaseNum,
@@ -309,7 +310,8 @@ function stateMachine({ instructionNum, sessionNum, phaseNum, questNum, exp } = 
                     beforeFeedbackDuration: exp.beforeFeedbackDuration,
                     completeFeedback: exp.completeFeedback,
                     feedbackObj: exp.feedbackImg,
-                    outcomeType: 'osef',
+                    // outcomeType: 'osef',
+                    outcomeType: [1, 0][+([-1, 0].includes(sessionNum))],
                     imgObj: imgObj,
                     sessionNum: sessionNum,
                     phaseNum: phaseNum,
