c12c5d0b061c42ec3c8f54d798c7dcfb6c64765e
caropio <carolinepioger98@gmail.com>
2025-03-20 17:56:35 +0100

    optionNums put in loop to solve issue of block randomization

--- js/modules/exp.js
+++ js/modules/exp.js
@@ -425,10 +425,11 @@ export class ExperimentParameters {
         // Learning with no fixed conditions
         // ===================================================================== //
         let arrToFill = new Array(nSession).fill().map((x) => []);
-        let optionNums = shuffle(range(0, options[0].length-1));
+        // let optionNums = shuffle(range(0, options[0].length-1));
 
         for (let sessionNum = 0; sessionNum < nSession; sessionNum++) {
             LOOP: for (let repeatNum = 0; repeatNum < nRepeat; repeatNum++) {
+                let optionNums = shuffle(range(0, options[0].length-1)); // Inside the loop so that not the same order of blocks between sessions and between repeat
                 for (let count1 = 0; count1 < options[sessionNum].length; count1++) {
 
                     let optionNum1 = optionNums[count1];
@@ -494,11 +495,12 @@ export class ExperimentParameters {
         // ===================================================================== //
         let arrToFill = new Array(nSession).fill().map((x) => []);
         let nOption = options[0].length;
-        let optionNums = shuffle(range(0, nOption-1));
+        // let optionNums = shuffle(range(0, nOption-1));
         let lotteryNums = shuffle(range(0, this.lotteryCont.length-1));
         let catchTrials = shuffle(this._generateCatchTrialsTwoOptions());
         // debugger;
         for (let sessionNum = 0; sessionNum < nSession; sessionNum++) {
+            let optionNums = shuffle(range(0, nOption-1)); // Inside the loop so that not the same order of blocks between sessions
             LOOP1: for (let count1 = 0; count1 < nOption; count1++) {
                 let optionNum1 = optionNums[count1];
                 let tempArray = [];
